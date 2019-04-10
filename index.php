6c32cb44bbc8495137f3193cbcab2f86c56b3ca9
PeterCharalambous <43430879+PeterCharalambous@users.noreply.github.com>
2019-04-10 19:58:14 +1000

    Revert "Demonstration for Jack"

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (isset($_POST['click'])) {
 }
 </style>
 <head>
-    <title>Robot Control - Develop</title>
+    <title>Robot Control</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 </head>
